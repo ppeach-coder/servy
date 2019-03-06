bd2215535787d2d2945e184be225459602f339f2
Chris Kilmer <christopherkilmer@gmail.com>
2019-03-06 06:19:20 -0700

    Parse Request Line, Route and Response.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -7,20 +7,26 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+    %{ conv | resp_body: "Bears, Lions, Tigers" }
   end
 
   def format_response(conv) do
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{byte_size(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
