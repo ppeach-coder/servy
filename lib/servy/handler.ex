84cd2b8359156b1a5244f5d5cdef82bde153902b
Chris Kilmer <christopherkilmer@gmail.com>
2019-03-11 15:08:57 -0600

    Handlin post requests.

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -35,6 +35,15 @@ defmodule Servy.Handler do
     handle_page("form.html", conv)
   end
 
+  # name-Baloo&type=Brown
+  def route(%Conv{method: "POST", path: "/bears"} = conv) do
+    %{
+      conv
+      | status: 201,
+        resp_body: "Created a #{conv.params["type"]} bear named #{conv.params["name"]}"
+    }
+  end
+
   def route(%Conv{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
   end
@@ -159,6 +168,21 @@ Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
 
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts(response)
+
+request = """
+POST /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 21
+
+name=Baloo&type=Brown
 """
 
 response = Servy.Handler.handle(request)
--- lib/servy/parser.ex
+++ lib/servy/parser.ex
@@ -2,12 +2,18 @@ defmodule Servy.Parser do
   alias Servy.Conv
 
   def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
+    [top, param_string] = String.split(request, "\n\n")
 
-    %Conv{method: method, path: path}
+    [request_line | header_lines] = String.split(top, "\n")
+
+    [method, path, _] = String.split(request_line, " ")
+
+    params = parse_params(param_string)
+
+    %Conv{method: method, path: path, params: params}
+  end
+
+  def parse_params(params_string) do
+    params_string |> String.trim() |> URI.decode_query()
   end
 end
